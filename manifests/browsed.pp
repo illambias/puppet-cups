3427923f80b689b1c0f2333937c0506d824a1bbd
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2013-06-20 10:05:59 +0200

    Renamed class installing cups-browsed + cleaned code

---
+++ manifests/browsed.pp
@@ -0,0 +1,19 @@
+class cups::browsed {
+
+  # Since CUPS 1.6, there is a separate daemon responsible for discovering
+  # network printers using Avahi and optionally the legacy CUPS protocol.
+
+  validate_re ($::lsbdistcodename, '/sid|raring|saucy/', 'cups-browsed only exists with cups >= 1.6')
+
+  package {'cups-browsed':
+    ensure => present,
+  }
+
+  service {'cups-browsed':
+    ensure => running,
+    pattern => 'cups-browsed',
+    enable  => true,
+    require => Package ['cups-browsed'],
+  }
+
+}
