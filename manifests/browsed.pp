c5aaf851f1ba9b41ddbfa16c1e3016225a47aa71
Issey Llambias <issey.llambias@camptocamp.com>
2014-06-17 11:07:28 +0200

    cups::browsed compatibility with ubuntu trusty 14.04

--- manifests/browsed.pp
+++ manifests/browsed.pp
@@ -3,7 +3,7 @@ class cups::browsed {
   # Since CUPS 1.6, there is a separate daemon responsible for discovering
   # network printers using Avahi and optionally the legacy CUPS protocol.
 
-  validate_re ($::lsbdistcodename, 'sid|raring|saucy', 'cups-browsed only exists with cups >= 1.6')
+  validate_re ($::lsbdistcodename, 'sid|raring|saucy|trusty', 'cups-browsed only exists with cups >= 1.6')
 
   package {'cups-browsed':
     ensure => present,
